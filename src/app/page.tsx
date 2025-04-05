166cdb09eeb50e64fbadedf02b89ccf6b632ee1e
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-04-05 12:54:36 -0300

    🔥chore: Adding shadcnn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <h1 className="font-barlow text-3xl font-bold text-red-500">
         Welcome new project React.JS
       </h1>
-    </div>
+      <Button variant={"destructive"}>Click here</Button>
+      </div>
   );
 }
+
